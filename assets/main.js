54a864c34910440acfa9e86dc2ebc051748deb76
Patrick Barrett <PatrickBarrett@Exosite.com>
2014-07-18 15:47:14 -0500

    Allow Deep Linking, Docs in master and HTML in gh-pages

--- assets/main.js
+++ assets/main.js
@@ -1,37 +1,7 @@
 (function ($) {
   $(document).ready(
     function(){
-      $('.header ul a').on('click', function(event) {
-        event.preventDefault();
-        var href = $(this).attr('href');
-        href = href.replace('#', '');
-        var file = '';
-        Flatdoc.run({
-          fetcher: Flatdoc.file(href + '.md')
-        });
-      });
-
       $(document).on('flatdoc:ready', function() {
-        $('.content a').each(function() {
-          console.log($(this).attr('href'));
-        });
-
-        // make content links work in flatdoc
-        $('.content a').on('click', function(event) {
-          var href = $(this).attr('href');
-          if (href.indexOf('http') != 0) {
-            // don't touch anchor links
-            if (href.indexOf('#') != 0) {
-              event.preventDefault();
-              if (href.indexOf('/') === href.length - 1) {
-                Flatdoc.run({
-                  fetcher: Flatdoc.file(href + 'README.md')
-                });
-              }
-            } 
-          }
-        });
-
         // Add GitHub style anchors to headers.
         var anchors = [];
         $('.content h2,h3,h4,h5,h6').each(function(i,v){
@@ -50,6 +20,12 @@
             name: anchor
           }).appendTo(v);  
         });
+        
+        if(document.location.hash != ""){
+          $('html, body').animate({
+            scrollTop: $(document.location.hash).offset().top
+          }, 500);
+        }
       });
   });
 })(window.jQuery)
