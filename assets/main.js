44ac90f76d1aa602c6b8d180f724b056b6bf49ba
Patrick Barrett <PatrickBarrett@Exosite.com>
2014-07-18 13:34:17 -0500

    Added function to add GitHub style anchors to headers.

--- assets/main.js
+++ assets/main.js
@@ -10,17 +10,19 @@
           fetcher: Flatdoc.file(href + '.md')
         });
       });
+
       $(document).on('flatdoc:ready', function() {
         $('.content a').each(function() {
           console.log($(this).attr('href'));
         });
+
         // make content links work in flatdoc
         $('.content a').on('click', function(event) {
           var href = $(this).attr('href');
           if (href.indexOf('http') != 0) {
-            event.preventDefault();
             // don't touch anchor links
             if (href.indexOf('#') != 0) {
+              event.preventDefault();
               if (href.indexOf('/') === href.length - 1) {
                 Flatdoc.run({
                   fetcher: Flatdoc.file(href + 'README.md')
@@ -28,6 +30,25 @@
               }
             } 
           }
+        });
+
+        // Add GitHub style anchors to headers.
+        var anchors = [];
+        $('.content h2,h3,h4,h5,h6').each(function(i,v){
+          var anchor = v.textContent.replace(/[!@#$%^&*()=+<>;:'"\\\/]/g, "").replace(/ /g, "-").toLowerCase();
+          while(anchors.indexOf(anchor) != -1){
+            var nums = anchor.match(/-(\d+)$/);
+            if(nums !== null){
+              anchor = anchor.replace(/(\d+)$/, parseInt(nums[1]) + 1);
+            }else{
+              anchor = anchor + "-1";
+            }
+          }
+
+          anchors.push(anchor);
+          $('<a/>', {
+            name: anchor
+          }).appendTo(v);  
         });
       });
   });
